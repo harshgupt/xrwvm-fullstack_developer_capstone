18ad5a726649db4cb2e763c0c20b5e805fe3b241
Harsh Gupta <harshgupt91@gmail.com>
2026-01-02 03:08:33 -0500

    Login and Logout

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS=['localhost','https://author-ide.skills.network/render?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZF9pbnN0cnVjdGlvbnNfdXJsIjoiaHR0cHM6Ly9jZi1jb3Vyc2VzLWRhdGEuczMudXMuY2xvdWQtb2JqZWN0LXN0b3JhZ2UuYXBwZG9tYWluLmNsb3VkLy0xTjJ1WTdsZGsxSDFhZ3dJNVJMaHcvQWRkJTIwU3RhdGljJTIwUGFnZXMtTWFyay12MS5tZD90PTE3NjUzNTk3MDYiLCJ0b29sX3R5cGUiOiJ0aGVpYSIsImF0bGFzX2ZpbGVfaWQiOjQzOTUwOCwiYWRtaW4iOmZhbHNlLCJpYXQiOjE3NjUzNTk3MDd9.E4LHqy2Qk1rm8HMS1kAdWs-rS53H9g2mSPf2O1BJVlU']
-CSRF_TRUSTED_ORIGINS=['https://author-ide.skills.network/render?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZF9pbnN0cnVjdGlvbnNfdXJsIjoiaHR0cHM6Ly9jZi1jb3Vyc2VzLWRhdGEuczMudXMuY2xvdWQtb2JqZWN0LXN0b3JhZ2UuYXBwZG9tYWluLmNsb3VkLy0xTjJ1WTdsZGsxSDFhZ3dJNVJMaHcvQWRkJTIwU3RhdGljJTIwUGFnZXMtTWFyay12MS5tZD90PTE3NjUzNTk3MDYiLCJ0b29sX3R5cGUiOiJ0aGVpYSIsImF0bGFzX2ZpbGVfaWQiOjQzOTUwOCwiYWRtaW4iOmZhbHNlLCJpYXQiOjE3NjUzNTk3MDd9.E4LHqy2Qk1rm8HMS1kAdWs-rS53H9g2mSPf2O1BJVlU']
+CSRF_TRUSTED_ORIGINS=['https://author-ide.skills.network/render?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZF9pbnN0cnVjdGlvbnNfdXJsIjoiaHR0cHM6Ly9jZi1jb3Vyc2VzLWRhdGEuczMudXMuY2xvdWQtb2JqZWN0LXN0b3JhZ2UuYXBwZG9tYWluLmNsb3VkLy0xTjJ1WTdsZGsxSDFhZ3dJNVJMaHcvQWRkJTIwU3RhdGljJTIwUGFnZXMtTWFyay12MS5tZD90PTE3NjUzNTk3MDYiLCJ0b29sX3R5cGUiOiJ0aGVpYSIsImF0bGFzX2ZpbGVfaWQiOjQzOTUwOCwiYWRtaW4iOmZhbHNlLCJpYXQiOjE3NjUzNTk3MDd9.E4LHqy2Qk1rm8HMS1kAdWs-rS53H9g2mSPf2O1BJVlU',
+'https://*.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -62,7 +63,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static')
+            os.path.join(BASE_DIR,'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -137,6 +140,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
